b7db40f90d457c1b2590fdbd1496061a16ec624e
liu-jianhao <jianhaoliu17@email.com>
2018-12-10 23:05:55 +0800

    MinNumberInRotatedArray

---
+++ Interview/剑指offer/11_MinNumberInRotatedArray/MinNumberInRotatedArray.cpp.cpp
@@ -0,0 +1,39 @@
+// 面试题11：旋转数组的最小数字
+// 题目：把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+// 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如数组
+// {3, 4, 5, 1, 2}为{1, 2, 3, 4, 5}的一个旋转，该数组的最小值为1。
+
+
+class Solution {
+public:
+    int minNumberInRotateArray(vector<int> rotateArray) {
+        if(rotateArray.empty())
+        {
+            return 0;
+        }
+        
+        int low = 0;
+        int high = rotateArray.size()-1;
+        int mid = low;
+        while(rotateArray[low] >= rotateArray[high])
+        {
+            if(high - low == 1)
+            {
+                mid = high;
+                break;
+            }
+            
+            mid = (low + high) / 2;
+            if(rotateArray[mid] >= rotateArray[low])
+            {
+                low = mid;
+            }
+            else if(rotateArray[mid] <= rotateArray[high])
+            {
+                high = mid;
+            }
+        }
+        
+        return rotateArray[mid];
+    }
+};
